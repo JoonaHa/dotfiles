4d1d4b69c972236495b6d450276f45156b70c146
JoonaHa <joona.halonen@helsinki.fi>
2021-03-17 04:20:15 +0200

    Initial commit

---
+++ desktop/.bin/scripts/i3lock.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TEMPDIR=/tmp
+
+sh $HOME/.bin/scripts/induvidual_scrot.sh
+convert $TEMPDIR/head_0.png -scale 10% -scale 1000% $TEMPDIR/head_0.png
+[[ -f $HOME/Kuvat/dotfiles/qY1nKlP.png ]] && convert $TEMPDIR/head_0.png $HOME/Kuvat/dotfiles/qY1nKlP.png -gravity center -composite -matte $TEMPDIR/head_0.png
+if [ -e $TEMPDIR/head_1.png ]; then
+  convert $TEMPDIR/head_1.png -scale 10% -scale 1000% $TEMPDIR/head_1.png
+  [[ -f $HOME/Kuvat/dotfiles/qY1nKlP.png ]] && convert $TEMPDIR/head_1.png $HOME/Kuvat/dotfiles/qY1nKlP.png -gravity center -composite -matte $TEMPDIR/head_1.png
+  convert $TEMPDIR/head_0.png $TEMPDIR/head_1.png +append $TEMPDIR/screen.png
+else
+  mv $TEMPDIR/head_0.png $TEMPDIR/screen.png
+fi
+#dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
+i3lock -i $TEMPDIR/screen.png -p default
+
---
+++ desktop/.bin/scripts/induvidual_scrot.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+xdpyinfo -ext XINERAMA | sed '/^  head #/!d;s///' |
+while IFS=' :x@,' read i w h x y; do
+    import -window root -crop ${w}x$h+$x+$y /tmp/head_$i.png
+done
+
