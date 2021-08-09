8511403e3694c980ccdf4845b71e59c396301d1d
JoonaHa <32388752+JoonaHa@users.noreply.github.com>
2021-08-09 12:11:05 +0300

    Floating and waybar changes

--- Laptop/usr/bin/sway-run.sh
+++ Laptop/usr/bin/sway-run.sh
@@ -5,7 +5,7 @@
 #export MOZ_ENABLE_WAYLAND=1
 #export GDK_BACKEND="wayland"
 export XDG_SESSION_TYPE="wayland"
-export XDG_CURRENT_DESKTOP="sway"
+export XDG_CURRENT_DESKTOP="Unity"
 export ECORE_EVAS_ENGINE="wayland_egl"
 export ELM_ENGINE="wayland_egl"
 export SDL_VIDEODRIVER="wayland"      
