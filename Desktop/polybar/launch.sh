e3f3c3b44fef071ea3b8f70d82e923d5969bdd98
JoonaHa <joona.halonen@helsinki.fi>
2022-01-15 03:10:32 +0200

    Polybar font and multi monitor change

--- Desktop/polybar/launch.sh
+++ Desktop/polybar/launch.sh
@@ -7,4 +7,19 @@ killall -q polybar
 
 # Launch bar
 echo "---" | tee -a /tmp/polybar.log
-polybar bottom >>/tmp/polybar.log 2>&1 & disown
+
+if type "xrandr"; then
+  PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
+  OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+
+  export TRAY_POSITION=right
+  MONITOR=$PRIMARY polybar -r bottom >>/tmp/polybar.log 2>&1 & disown
+
+  for m in $OTHERS; do
+    export TRAY_POSITION=none
+    MONITOR=$m polybar -r bottom >>/tmp/polybar.log 2>&1 & disown
+  done
+
+else
+  polybar -r bottom >>/tmp/polybar.log 2>&1 & disown
+fi
