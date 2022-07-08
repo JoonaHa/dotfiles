b3229782fe234a62dd621116668215e5d2398e11
JoonaHa <joona.halonen@helsinki.fi>
2022-07-09 00:02:36 +0300

    Scratchpad bind and lock screen cleanup

--- scripts/scripts/i3lock.sh
+++ scripts/scripts/i3lock.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 TEMPDIR=/tmp
 
@@ -12,7 +12,6 @@ convert $TEMPDIR/head_0.png -scale 5% -scale 2000% $TEMPDIR/head_0.png
 if [ -e $TEMPDIR/head_1.png ]; then
   convert $TEMPDIR/head_1.png -scale 5% -scale 2000% $TEMPDIR/head_1.png
 #  [[ -f $HOME/Kuvat/dotfiles/qY1nKlP.png ]] && convert $TEMPDIR/head_1.png $HOME/Kuvat/dotfiles/qY1nKlP.png -gravity center -composite -matte $TEMPDIR/head_1.png
-  #convert $TEMPDIR/head_0.png $TEMPDIR/head_1.png +append $TEMPDIR/screen.png
   convert +append -gravity center $TEMPDIR/head_0.png $TEMPDIR/head_1.png $TEMPDIR/screen.png
 else
   mv $TEMPDIR/head_0.png $TEMPDIR/screen.png
