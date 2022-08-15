7202f9e0033c3d29f913bfabc6b8b3ed6fc7f996
JoonaHa <joona.halonen@helsinki.fi>
2022-08-15 11:23:35 +0300

    Nvim: syntax

--- nvim/.config/nvim/lua/user/formatting.lua
+++ nvim/.config/nvim/lua/user/formatting.lua
@@ -2,7 +2,7 @@ local formatting_instance = {}
 
 function formatting_instance.init(formatters)
 	local builtin_sources = {}
-	for i, x in ipairs(formatters) do
+	for _, x in ipairs(formatters) do
 		local callable = "return require('null-ls').builtins." .. x
 		--for w in x:gmatch('a%+') do table.insert(fields, w) end
 		 table.insert(builtin_sources, load(callable)())
