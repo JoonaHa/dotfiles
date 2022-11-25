c445fc600c2ea972a36b6f67f9fd1240c1603c75
JoonaHa <32388752+JoonaHa@users.noreply.github.com>
2022-11-25 20:09:01 +0200

    Add script for LG devmode

---
+++ scripts/bin/scripts/renew_lg_devmode.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+dir=$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)
+if [ ! -f "${dir}/.token" ]
+then
+    echo "$(date --iso-8601=seconds): Can not find token file ${dir}/.token" | tee ${dir}/lg_renew.log
+    exit 1 
+fi
+
+token=$(cat "${dir}/.token")
+
+url="https://developer.lge.com/secure/ResetDevModeSession.dev?sessionToken=${token}"
+return=$(curl -L "$url")
+touch ${dir}/lg_renew.log
+echo "$(date --iso-8601=seconds): $return" | tee ${dir}/lg_renew.log
