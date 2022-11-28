c4fd8325a8bdaafd751e058fe4aef39309f90e19
JoonaHa <32388752+JoonaHa@users.noreply.github.com>
2022-11-28 11:19:19 +0200

    Script: lg log file touch before new logging

--- scripts/bin/scripts/renew_lg_devmode.sh
+++ scripts/bin/scripts/renew_lg_devmode.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 dir=$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)
+touch ${dir}/lg_renew.log
+
 if [ ! -f "${dir}/.token" ]
 then
     echo "$(date --iso-8601=seconds): Can not find token file ${dir}/.token" | tee ${dir}/lg_renew.log
@@ -11,5 +13,4 @@ token=$(cat "${dir}/.token")
 
 url="https://developer.lge.com/secure/ResetDevModeSession.dev?sessionToken=${token}"
 return=$(curl -L "$url")
-touch ${dir}/lg_renew.log
 echo "$(date --iso-8601=seconds): $return" | tee ${dir}/lg_renew.log
