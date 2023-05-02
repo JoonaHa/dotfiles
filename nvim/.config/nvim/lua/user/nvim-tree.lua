4688bfe34a3f536bef48755883d38936fd608141
JoonaHa <32388752+JoonaHa@users.noreply.github.com>
2023-05-02 18:58:56 +0300

    Nvim: Update deprecated nvim-tree functions

--- nvim/.config/nvim/lua/user/nvim-tree.lua
+++ nvim/.config/nvim/lua/user/nvim-tree.lua
@@ -4,7 +4,29 @@ if not status_ok then
   return
 end
 
+local function custom_on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  api.config.mappings.default_on_attach(bufnr)
+
+  vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', 'c', api.node.navigate.parent_close, opts('Close Directory'))
+  vim.keymap.set('n', 'v', api.node.open.vertical, opts('Open: Vertical Split'))
+  vim.keymap.set('n', 'P', function()
+    local node = api.tree.get_node_under_cursor()
+    print(node.absolute_path)
+  end, opts('Print Node Path'))
+
+end
+
 nvim_tree.setup {
+  on_attach = custom_on_attach,
   sort_by = "case_sensitive",
   hijack_directories = {
     enable = true,
@@ -20,7 +42,6 @@ nvim_tree.setup {
   -- },
   disable_netrw = false,
   hijack_netrw = false,
-  open_on_setup = false,
   update_cwd = true,
 
   renderer = {
@@ -50,14 +71,6 @@ nvim_tree.setup {
     adaptive_size = true,
     hide_root_folder = false,
     side = "left",
-    mappings = {
-      custom_only = false,
-      list = {
-        { key = { "l", "<CR>", "o" }, action ="edit" },
-        { key = "c", action ="close_node" },
-        { key = "v", action="vsplit" },
-      },
-    },
     number = false,
     relativenumber = false,
   },
