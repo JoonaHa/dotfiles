d5b0ef6e047bacbf28e69772a5e5584a1b103ee3
JoonaHa <32388752+JoonaHa@users.noreply.github.com>
2024-05-31 17:44:53 +0300

    Nvim cmp <cr> select disable, Add preset mappings for cmdline

--- nvim/.config/nvim/lua/user/completion/cmp.lua
+++ nvim/.config/nvim/lua/user/completion/cmp.lua
@@ -117,7 +117,7 @@ function cmp_instance.init()
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
       ['<Tab>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
@@ -150,6 +150,7 @@ function cmp_instance.init()
   })
 
   cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
     sources = cmp.config.sources({
       { name = 'path' }
     }, {
