287cb83e5aa86da75f505f306fb2fb4395e88813
JoonaHa <32388752+JoonaHa@users.noreply.github.com>
2024-09-18 17:54:42 +0300

    Nvim: Add context binds to treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -47,6 +47,17 @@ return {
               ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
               -- You can also use captures from other query groups like `locals.scm`
               ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+            },      -- You can choose the select mode (default is charwise 'v')
+            --
+            -- Can also be a function which gets passed a table with the keys
+            -- * query_string: eg '@function.inner'
+            -- * method: eg 'v' or 'o'
+            -- and should return the mode ('v', 'V', or '<c-v>') or a table
+            -- mapping query_strings to modes.
+            selection_modes = {
+              ['@parameter.outer'] = 'v', -- charwise
+              ['@function.outer'] = 'V', -- linewise
+              ['@class.outer'] = '<c-q>', -- blockwise
             },
             -- If you set this to `true` (default is `false`) then any textobject is
             -- extended to include preceding or succeeding whitespace. Succeeding
@@ -83,6 +94,9 @@ return {
             goto_previous_start = {
               ["[n"] = "@function.outer",
               ["[["] = "@class.outer",
+              ["[o"] = "@loop.*",
+              ["[s"] = { query = "@scope", query_group = "locals", desc = "Previous scope" },
+              ["[z"] = { query = "@fold", query_group = "folds", desc = "Previous fold" },
             },
             goto_previous_end = {
               ["[M"] = "@function.outer",
