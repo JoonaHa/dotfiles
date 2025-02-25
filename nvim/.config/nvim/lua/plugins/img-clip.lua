adedf80b617b09de648fbe809f495b02c12b45f1
JoonaHa <32388752+JoonaHa@users.noreply.github.com>
2025-02-25 20:07:41 +0200

    Nvim: Better image-clip template

--- nvim/.config/nvim/lua/plugins/img-clip.lua
+++ nvim/.config/nvim/lua/plugins/img-clip.lua
@@ -11,6 +11,7 @@ return {
         },
         quarto = {
           url_encode_path = true,
+          template = '![$CURSOR]($FILE_PATH)',
           drag_and_drop = {
             download_images = false,
           },
