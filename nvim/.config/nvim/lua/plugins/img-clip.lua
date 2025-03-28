22306d7e4512725ac61bd3285281d16a03a16f34
JoonaHa <32388752+JoonaHa@users.noreply.github.com>
2025-03-28 18:03:45 +0200

    Nvim: Fix img-clip settings

--- nvim/.config/nvim/lua/plugins/img-clip.lua
+++ nvim/.config/nvim/lua/plugins/img-clip.lua
@@ -1,24 +1,25 @@
 return {
   "HakonHarnes/img-clip.nvim",
   event = "VeryLazy",
-    ft = { 'markdown', 'quarto', 'latex' },
-      filetypes = {
-        markdown = {
-          url_encode_path = true,
-          drag_and_drop = {
-            download_images = false,
-          },
-        },
-        quarto = {
-          url_encode_path = true,
-          template = '![$CURSOR]($FILE_PATH)',
-          drag_and_drop = {
-            download_images = false,
-          },
+  ft = { 'markdown', 'quarto', 'latex' },
+  keys = {
+    { '<leader>ii', '<cmd>PasteImage<cr>', desc = 'insert [i]mage from clipboard' },
+  },
+  opts = {
+    filetypes = {
+      markdown = {
+        url_encode_path = true,
+        drag_and_drop = {
+          download_images = false,
         },
       },
-    config = function(_, opts)
-      require('img-clip').setup(opts)
-      vim.keymap.set('n', '<leader>ii', ':PasteImage<cr>', { desc = 'insert [i]mage from clipboard' })
-    end,
+      quarto = {
+        url_encode_path = true,
+        template = '![$CURSOR]($FILE_PATH)',
+        drag_and_drop = {
+          download_images = false,
+        },
+      },
+    }
+  }
 }
