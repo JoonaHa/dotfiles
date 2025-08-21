1a2053ac3fb112c4493b3a105f1bbbd4d890b210
JoonaHa <32388752+JoonaHa@users.noreply.github.com>
2025-08-21 13:45:44 +0300

    Pin cmp-nvim-lsp

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -4,7 +4,7 @@ return {
     lazy = false,
     dependencies = {
       "onsails/lspkind.nvim",
-      "hrsh7th/cmp-nvim-lsp",
+      {"hrsh7th/cmp-nvim-lsp", commit = "a8912b8"},
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-cmdline",
