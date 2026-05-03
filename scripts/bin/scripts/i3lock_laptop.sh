3fa1f64fb2b9a5fd3d840dcf48a796210a73fdd4
JoonaHa <32388752+JoonaHa@users.noreply.github.com>
2026-05-03 20:48:10 +0300

    Scripts: Clean up

--- scripts/bin/scripts/i3lock_laptop.sh
+++ scripts/bin/scripts/i3lock_laptop.sh
@@ -2,7 +2,11 @@
 
 TEMPDIR=/tmp
 
-sh $HOME/.bin/scripts/induvidual_scrot.sh
+xdpyinfo -ext XINERAMA | sed '/^  head #/!d;s///' |
+while IFS=' :x@,' read i w h x y; do
+    grim -c -g "$x,$y ${w}x$h" /tmp/head_$i.png
+#    import -window root -crop ${w}x$h+$x+$y /tmp/head_$i.png
+done
 convert $TEMPDIR/head_0.png -scale 10% -scale 1000% $TEMPDIR/head_0.png
 [[ -f $HOME/.bin/scripts/lock.png ]] && convert $TEMPDIR/head_0.png $HOME/.bin/scripts/lock.png  -gravity center -composite -matte $TEMPDIR/head_0.png
 if [ -e $TEMPDIR/head_1.png ]; then
