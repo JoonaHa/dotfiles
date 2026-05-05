d8780a3228fd98bae2852650dd70cf6cb594b9f8
JoonaHa <32388752+JoonaHa@users.noreply.github.com>
2026-05-05 20:04:59 +0300

    Scripts: More error-handling for swaylock

--- scripts/bin/scripts/swaylock_multimonitor-laptop.sh
+++ scripts/bin/scripts/swaylock_multimonitor-laptop.sh
@@ -3,14 +3,14 @@
 
 LOCKARGS=""
 NORM=""
-MONITORS=$(swaymsg -t get_outputs | jq -r '.[] | select(.active == true) | .name')
-for OUTPUT in $MONITORS
+for OUTPUT in $(swaymsg -t get_outputs | jq -r '.[] | select(.active == true) | .name')
+
 do
     IMAGE=/tmp/$OUTPUT-lock.png
     grim -o "$OUTPUT" "$IMAGE"
-    convert "$IMAGE" -scale 10% -scale 1000% "$IMAGE"
     if [ -f $IMAGE ]
     then 
+        convert "$IMAGE" -scale 10% -scale 1000% "$IMAGE"
         LOCKARGS="${LOCKARGS} --image ${OUTPUT}:${IMAGE}"
         IMAGES="${IMAGES} ${IMAGE}"
     else
